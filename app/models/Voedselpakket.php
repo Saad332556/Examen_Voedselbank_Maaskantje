c2e1a0bddafaf2faf868fafc7d23ae8478885a9f
Saad332556 <332556@student.mboutrecht.nl>
2023-06-20 15:26:05 +0200

    create werkt!

--- app/models/Voedselpakket.php
+++ app/models/Voedselpakket.php
@@ -48,7 +48,8 @@ class Voedselpakket
 
     public function deleteVoedselpakket($id)
     {
-        $this->db->query("DELETE * FROM Voedselpakket 
+        $this->db->query("DELETE Voedselpakket, Product, Klant 
+                          FROM Voedselpakket 
                           INNER JOIN Product ON Voedselpakket.id = Product.voedselpakket_id 
                           INNER JOIN Klant ON Voedselpakket.klant_id = Klant.id 
                           WHERE Voedselpakket.id = :id");
@@ -58,24 +59,23 @@ class Voedselpakket
 
     public function createVoedselpakket($post)
     {
-        $this->db->query("INSERT INTO Voedselpakket (Voedselpakket.pakketnummer, 
-                                                     Voedselpakket.datum_samenstelling, 
-                                                     Voedselpakket.datum_uitgifte, 
-                                                     Product.aantal, 
-                                                     Klant.naam)
-                          SELECT                    :pakketnummer,
-                                                    :datum_samenstelling,
-                                                    :datum_uitgifte,
-                                                    :aantal,
-                                                    :naam
-                                                    FROM Voedselpakket
-                                                    INNER JOIN Product ON Voedselpakket.id = Product.voedselpakket_id 
-                                                    INNER JOIN Klant ON Voedselpakket.klant_id = Klant.id ");
+
+        $this->db->query("INSERT INTO Klant (naam) VALUES (:naam)");
+        $this->db->bind(':naam', $post['naam'], PDO::PARAM_STR);
+        $this->db->execute();
+
+        $this->db->query("INSERT INTO Voedselpakket (klant_id, pakketnummer, datum_samenstelling, datum_uitgifte)
+                          VALUES (LAST_INSERT_ID(), :pakketnummer, :datum_samenstelling, :datum_uitgifte)");
         $this->db->bind(':pakketnummer', $post['pakketnummer'], PDO::PARAM_INT);
         $this->db->bind(':datum_samenstelling', $post['datum_samenstelling'], PDO::PARAM_STR);
         $this->db->bind(':datum_uitgifte', $post['datum_uitgifte'], PDO::PARAM_STR);
+        $this->db->execute();
+
+        $this->db->query("INSERT INTO Product (voedselpakket_id, aantal) VALUES (LAST_INSERT_ID(), :aantal)");
         $this->db->bind(':aantal', $post['aantal'], PDO::PARAM_INT);
-        return $this->db->execute();
+        $this->db->execute();
+
+        return true;
     }
 
 }
