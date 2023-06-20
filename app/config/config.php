974bd3ab08c4afc2df88f9a94a67fc9c60489506
Lazarscorro <90773909+Lazarscorro@users.noreply.github.com>
2023-06-20 13:25:32 +0200

    config
    de config routing

---
+++ app/config/config.php
@@ -0,0 +1,11 @@
+<?php
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS', '');
+    define('DB_NAME', 'Voedselbank');
+
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    define('URLROOT', 'http://eindexamen.com');
+
+    define('SITENAME', 'examenklant');
